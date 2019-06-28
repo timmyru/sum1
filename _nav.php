7eb4c6eb5f6cc1c52292c4c49f541ba9fd8786b9
timmyru <33581137+timmyru@users.noreply.github.com>
2019-06-28 13:55:02 +0300

    Add files via upload

--- _nav.php
+++ _nav.php
@@ -6,6 +6,7 @@ session_start();
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav">
             <a class="nav-item nav-link" href="index.php">Главная</a>
+            <a class="nav-item nav-link pl-5" href="manager-login.php"><?= isset($_SESSION['manager.login']) ? 'Список заявок для менеджеров' : 'Вход для менеджеров'; ?></a>
             <a class="nav-item nav-link pl-5" href="admin-login.php"><?= isset($_SESSION['login']) ? 'Админка' : 'Вход для администраторов'; ?></a>
         </div>
     </div>
